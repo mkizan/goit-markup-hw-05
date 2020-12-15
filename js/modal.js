c0a582c12168c93789f080e792a7b53a3391e443
Mykola Kizan <mkizan@gmail.com>
2020-12-15 07:45:19 +0200

    fixed modal

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-const ref = {
+const refs = {
     openModalBtn: document.querySelector("[data-open-modal]"),
     closeModalBtn: document.querySelector("[data-close-modal]"),
     backdrop: document.querySelector("[data-backdrop]"),
